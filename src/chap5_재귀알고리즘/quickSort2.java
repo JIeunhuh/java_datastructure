c29217cc7cbb3b62e3baa4ec242d258d0049b010
Jieun Huh <entks301@naver.com>
2023-06-03 16:35:41 +0900

    Committing local changes before pull

---
+++ src/chap5_재귀알고리즘/quickSort2.java
@@ -0,0 +1,64 @@
+package chap5_재귀알고리즘;
+
+import java.util.Random;
+
+class QuickSort2 {
+    // --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---//
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    // --- 퀵 정렬(비재귀 버전)---//
+    static void quickSort(int[] a, int left, int right) {
+        Stack2 st = new Stack2(right - left + 1);
+        Point1 p = new Point1(left, right);
+        st.push(p);
+
+        while (st.isEmpty() != true) {
+            p = st.pop();
+            int pl = left = p.GetX(); // 왼쪽 커서
+            int pr = right = p.GetY(); // 오른쪽 커서
+            int x = a[(left + right) / 2]; // 피벗은 가운데 요소
+
+            do {
+                while (a[pl] < x)
+                    pl++;
+                while (a[pr] > x)
+                    pr--;
+                if (pl <= pr)
+                    swap(a, pl++, pr--);
+            } while (pl <= pr);
+
+            if (left < pr) {
+                p = new Point1(left, pr);
+                st.push(p);
+            }
+            if (pl < right) {
+                p = new Point1(pl, right);
+                st.push(p);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        System.out.println("퀵 정렬(버전 1)");
+        int nx = 100;
+        int[] x = new int[nx];
+        for (int i = 0; i < nx; i++) {
+            x[i] = random.nextInt(10);
+            System.out.println(x[i]);
+        }
+        System.out.println("=".repeat(50));
+
+        quickSort(x, 0, nx - 1); // 배열 x를 퀵정렬
+
+        System.out.println("오름차순으로 정렬했습니다.");
+        for (int i = 0; i < nx; i++)
+            System.out.println("x[" + i + "]=" + x[i]);
+    }
+}
